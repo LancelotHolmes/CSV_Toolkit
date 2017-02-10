ddfadade62b70c28c882690c89b5d0dafe2e2f94
LancelotHolmes <494852624@qq.com>
2017-02-10 11:03:20 +0800

    add new csv match method match multiple column values

--- csv_toolkit/Csv_Match.py
+++ csv_toolkit/Csv_Match.py
@@ -5,11 +5,14 @@ import csv
 csv.field_size_limit(sys.maxint)
 
 # write to common csv file with delimiter ','
-# output the rows with matched id in id_list to a new csv file
-def csv_match(id_list,key,input_file,output_file):
+# output the rows with matched key in refer_list to a new csv file
+# @params
+# refer_list: the list referred to
+# key,key2: column name of csv file to check the value in the refer_list or not
+def csv_match(refer_list,key,input_file,output_file):
     with open(input_file, 'rb') as f:
         reader = csv.DictReader(f)
-        rows = [row for row in reader if row[key] in set(id_list)]
+        rows = [row for row in reader if row[key] in set(refer_list)]
 
     header = rows[0].keys()
     with open(output_file, 'w') as f:
@@ -19,11 +22,32 @@ def csv_match(id_list,key,input_file,output_file):
             f.write(",".join(data[h] for h in header))
             f.write('\n')
 
-# output the rows with not matched id in id_list to a new csv file
-def csv_not_match(id_list, key, input_file, output_file):
+# output the rows with matched key1 or key2 in refer_list to a new csv file
+# @params
+# refer_list: the list referred to
+# key,key2: column name of csv file to check the value in the refer_list or not
+def csv_match2(refer_list, key1, key2, input_file, output_file):
     with open(input_file, 'rb') as f:
         reader = csv.DictReader(f)
-        rows = [row for row in reader if not row[key] in set(id_list)]
+        rows = [row for row in reader if (row[key1] in set(refer_list)) or (row[key2] in set(refer_list))]
+
+    header = rows[0].keys()
+    with open(output_file, 'w') as f:
+        f.write(','.join(header))
+        f.write('\n')
+        for data in rows:
+            f.write(",".join(data[h] for h in header))
+            f.write('\n')
+
+
+# output the rows that not matched with key in refer_list to a new csv file
+# @params
+# refer_list: the list referred to
+# key,key2: column name of csv file to check the value in the refer_list or not
+def csv_not_match(refer_list, key, input_file, output_file):
+    with open(input_file, 'rb') as f:
+        reader = csv.DictReader(f)
+        rows = [row for row in reader if not row[key] in set(refer_list)]
 
     header = rows[0].keys()
     with open(output_file, 'w') as f:
